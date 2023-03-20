72cad2b04c375bfd4c8b645c82a47defbadd6c98
Andie.Phan <andie.phan@analog.com>
2023-03-20 09:59:06 -0700

    session table build files

---
+++ stogui/session_table/frontend/build/precache-manifest.2e03f9bdc88bd7ac68848e9dd32df785.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd1f21f928b71bbc6ffc826f1667352",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1276e701015af9292f86",
+    "url": "./static/js/2.3df4f37b.chunk.js"
+  },
+  {
+    "revision": "0ef489908920fd5df994a2825834bd4c",
+    "url": "./static/js/2.3df4f37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3eccab67ea7d4a99e46",
+    "url": "./static/js/main.d613eff9.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "99c6de9522e46c9c69649216e988ae5e",
+    "url": "./static/media/IBMPlexSans-Bold.99c6de95.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "./static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "./static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "1ca9107e7544d3424419585c7c84cb67",
+    "url": "./static/media/IBMPlexSans-SemiBold.1ca9107e.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "./static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
