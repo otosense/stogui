c55359edba69dc435bc731abe3b946eaacc4accc
Andie.Phan <andie.phan@analog.com>
2023-08-01 14:40:55 -0700

    demo app and dag builder

---
+++ demo/app.py
@@ -0,0 +1,71 @@
+from collections.abc import Callable
+from dataclasses import dataclass
+import subprocess
+
+from front import APP_KEY, RENDERING_KEY, ELEMENT_KEY, NAME_KEY
+from front.elements import InputBase
+from streamlitfront.base import mk_app
+
+from stogui.drag_and_drop_flow import drag_and_drop_flow
+from stogui.pipeline_maker import pipeline_maker
+
+
+def drag_and_drop_dag_maker(x):
+    x
+
+
+@dataclass
+class DnDFlow(InputBase):
+    def render(self):
+        return drag_and_drop_flow()
+
+
+def mk_pipeline(steps):
+    return steps
+
+
+@dataclass
+class PipelineMaker(InputBase):
+    items: list = None
+    steps: list = None
+    serializer: Callable = None
+
+    def render(self):
+        return pipeline_maker(
+            items=self.items, steps=self.steps, serializer=self.serializer,
+        )
+
+
+def main():
+    app = mk_app(
+        [drag_and_drop_dag_maker, mk_pipeline],
+        config={
+            APP_KEY: {'title': 'STOGUI Demo'},
+            RENDERING_KEY: {
+                'drag_and_drop_dag_maker': {
+                    'execution': {'inputs': {'x': {ELEMENT_KEY: DnDFlow}}}
+                },
+                'mk_pipeline': {
+                    NAME_KEY: 'Pipeline Maker',
+                    'execution': {
+                        'inputs': {
+                            'steps': {
+                                ELEMENT_KEY: PipelineMaker,
+                                'items': ['chunker', 'featurizer'],
+                                'serializer': lambda x: x,
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    )
+    app()
+
+
+def run_streamlit_app():
+    subprocess.call(['streamlit', 'run', __file__, '--server.headless=true'])
+
+
+if __name__ == '__main__':
+    main()
--- stogui/drag_and_drop_flow/frontend/src/StDragAndDropFlow.tsx
+++ stogui/drag_and_drop_flow/frontend/src/StDragAndDropFlow.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect } from 'react';
 import { Streamlit } from "streamlit-component-lib"
-import { DnDFlow } from '@otosense/ogui/lib/DAG/Components/DragandDrop/Dagger';
-
+import { DnDFlow } from '@otosense/ogui/lib/DAGv2/Components/DragandDrop/Dagger';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// const queryClient = new QueryClient();
 const StDragAndDropFlow = () => {
   useEffect(() => Streamlit.setFrameHeight());
 
-  return <DnDFlow/>
+  return (
+    // <QueryClientProvider client={queryClient}>
+    <DnDFlow />
+    // </QueryClientProvider>
+  )
 }
 
 export default StDragAndDropFlow;
