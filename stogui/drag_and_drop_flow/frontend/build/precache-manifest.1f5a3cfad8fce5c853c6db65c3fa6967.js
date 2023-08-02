a0d6159f3393accd74710d12dad315cd783a18a3
Andie.Phan <andie.phan@analog.com>
2023-08-02 14:19:14 -0700

    dndflow

---
+++ stogui/drag_and_drop_flow/frontend/build/precache-manifest.1f5a3cfad8fce5c853c6db65c3fa6967.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673066bec58b91ed582653c21985ac18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca025a0fa3dc98fdcda1",
+    "url": "./static/css/2.5c830768.chunk.css"
+  },
+  {
+    "revision": "ca025a0fa3dc98fdcda1",
+    "url": "./static/js/2.820d312e.chunk.js"
+  },
+  {
+    "revision": "b99a490bb0b20150eb30fbc2ea1fad65",
+    "url": "./static/js/2.820d312e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa7603a2e319389e4b5",
+    "url": "./static/js/main.d8453dfc.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "e95174f1aec95fe7d516319af3c82fca",
+    "url": "./static/media/files.e95174f1.png"
+  }
+]);
--- stogui/drag_and_drop_flow/frontend/src/StDragAndDropFlow.tsx
+++ stogui/drag_and_drop_flow/frontend/src/StDragAndDropFlow.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react';
 import { Streamlit } from "streamlit-component-lib"
-import { DnDFlow } from '@otosense/ogui/lib/DAGv2/Components/DragandDrop/Dagger';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// const queryClient = new QueryClient();
+import { DnDFlow } from '@otosense/ogui/lib/DAGv1/Components/DragandDrop/Dagger';
+
 const StDragAndDropFlow = () => {
   useEffect(() => Streamlit.setFrameHeight());
 
-  return (
-    // <QueryClientProvider client={queryClient}>
-    <DnDFlow />
-    // </QueryClientProvider>
-  )
+  return (<DnDFlow />)
 }
 
 export default StDragAndDropFlow;
